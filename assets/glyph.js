24a8535097159b9e216e04971dfed30a62ea02bc
GJTL <glennon@pugetsound.edu>
2016-01-02 20:07:32 -0800

    Part 4
    Added Entity system, and mixin support.

--- assets/glyph.js
+++ assets/glyph.js
@@ -1,8 +1,9 @@
-Game.Glyph = function(chr, foreground, background) {
+Game.Glyph = function(properties) {
   //Instantiate properties to default if there weren't passed
-  this._char = chr || ' ';
-  this._foreground = foreground || 'white';
-  this._background = background || 'black';
+  properties = properties || {};
+  this._char = properties['character'] || ' ';
+  this._foreground = properties['foreground'] || 'white';
+  this._background = properties['background'] || 'black';
 };
 
 //Create standard letters for glyphs
--- assets/tile.js
+++ assets/tile.js
@@ -1,11 +1,31 @@
-Game.Tile = function(glyph) {
-this._glyph = glyph;
+Game.Tile = function(properties) {
+    properties = properties || {};
+    //call the Glyph constructor w/our properties
+    Game.Glyph.call(this, properties);
+    //set up the properties. We use false by default.
+    this._isWalkable = properties['isWalkable'] || false;
+    this._isDiggable = properties['isDiggable'] || false;
 };
 
-Game.Tile.prototype.getGlyph = function() {
-  return this._glyph;
-};
+//Make tiles inherit all Glyph functionality
+Game.Tile.extend(Game.Glyph);
+
+//Yo str8 up basic gettas
+Game.Tile.prototype.isWalkable = function() {
+  return this._isWalkable;
+}
+Game.Tile.prototype.isDiggable = function() {
+  return this._isDiggable;
+}
+
 
 Game.Tile.nullTile = new Game.Tile(new Game.Glyph());
-Game.Tile.floorTile = new Game.Tile(new Game.Glyph('.'));
-Game.Tile.wallTile = new Game.Tile(new Game.Glyph('#', 'goldenrod'));
+Game.Tile.floorTile = new Game.Tile({
+    character: '.',
+    isWalkable: true
+});
+Game.Tile.wallTile = new Game.Tile({
+    character: '#',
+    foreground: 'goldenrod',
+    isDiggable: true
+});
